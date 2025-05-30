5e95b7a12b79dd0608d5fe391019202ee79155e8
Christian <cstryi@freenet.de>
2025-05-30 19:51:45 +0200

    Alle Main Buttons

---
+++ app/src/main/java/de/stryi/vorratsuebersicht2/MainActivity.kt
@@ -0,0 +1,26 @@
+package de.stryi.vorratsuebersicht2
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import de.stryi.vorratsuebersicht2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        button = findViewById(R.id.MainButton_Kategorie)
+        button.setOnClickListener {
+
+            val fragment = FirstFragment()
+        }
+    }
+}
