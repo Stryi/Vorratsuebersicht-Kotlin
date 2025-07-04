2c449e3fb689759b369358d8879648b7fb5ecd7a
Christian <cstryi@freenet.de>
2025-07-04 10:21:29 +0200

    Berechtigung für Kamera anfordern.

--- app/src/main/java/de/stryi/vorratsuebersicht2/MainActivity.kt
+++ app/src/main/java/de/stryi/vorratsuebersicht2/MainActivity.kt
@@ -1,8 +1,11 @@
 package de.stryi.vorratsuebersicht2
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
 import androidx.appcompat.app.AppCompatActivity
 import de.stryi.vorratsuebersicht2.databinding.ActivityMainBinding
 
@@ -11,12 +14,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var buttonArtikel: Button
     private lateinit var buttonEanCodeScan: Button
+    private val CAMERA_PERMISSION_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         AndroidDatabase.restoreDatabasesFromResourcesOnStartup(this)
 
@@ -30,15 +35,35 @@ class MainActivity : AppCompatActivity() {
 
         buttonEanCodeScan = findViewById(R.id.MainButton_Barcode)
         buttonEanCodeScan.setOnClickListener {
-
-            val text = "EAN und QR-Code Scan"
-            val eanScanFragment = EanCodeScan.newInstance(text, "TEST")
-
-            // Fragment modal anzeigen
-            eanScanFragment.show(supportFragmentManager, "EanCodeScan")
-
+            requestCameraPermission()
         }
 
 
+    }
+
+    private fun requestCameraPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        } else {
+            // Permission has already been granted
+            openEanCodeScanner()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permission was granted
+                openEanCodeScanner()
+            } else {
+                // Permission denied. Handle the case appropriately.
+            }
+        }
+    }
+
+    private fun openEanCodeScanner() {
+        val eanScanFragment = EanCodeScan.newInstance("EAN und QR-Code Scan", "TEST")
+        eanScanFragment.show(supportFragmentManager, "EanCodeScan")
     }
 }
