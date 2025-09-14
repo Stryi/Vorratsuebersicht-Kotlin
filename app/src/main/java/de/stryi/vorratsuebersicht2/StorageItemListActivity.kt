555cee3ab70c678a472e1fafbdfa2cbcf1b55778
Christian <cstryi@freenet.de>
2025-09-14 21:28:41 +0200

    Toolbar jetzt sichtbar

--- app/src/main/java/de/stryi/vorratsuebersicht2/StorageItemListActivity.kt
+++ app/src/main/java/de/stryi/vorratsuebersicht2/StorageItemListActivity.kt
@@ -1,6 +1,9 @@
 package de.stryi.vorratsuebersicht2
 
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.View
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -17,19 +20,35 @@ class StorageItemListActivity : AppCompatActivity() {
         binding = StorageItemList2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //this.setSupportActionBar(binding.StorageItemListAppBar)
-        this.setSupportActionBar(binding.toolbar)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+        this.setSupportActionBar(binding.StorageItemListAppBar)
 
         binding.StorageItemListText.text = "Das ist ein Test."
 
         this.title = "TEST"
 
-        binding.toolbarLayout.title = title
+        //binding.toolbarLayout.title = title
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
     }
+
+    /*
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+    */
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
+        menuInflater.inflate(R.menu.storage_item_list_menu, menu)
+        return true
+    }
+
+
 }
